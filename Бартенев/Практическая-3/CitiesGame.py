e9ec4e01c69fa5e5cd1bb5b49fb85a12d2cbfbf6
Flamer322 <b.bar1enev@gmail.com>
2021-10-04 19:52:08 +0300

    Added Watch

--- Бартенев/Практическая-3/CitiesGame.py
+++ Бартенев/Практическая-3/CitiesGame.py
@@ -20,3 +20,35 @@ class CitiesDict:
             return True
         else:
             return False
+
+    def get_next_char(self, city):
+        city = city.upper()
+        if city[-1] in ['Ы', 'Й', 'Ь', 'Ъ']:
+            return city[-2]
+        else:
+            return city[-1]
+
+
+class Watch:
+    def __init__(self):
+        self.turns_time = []
+        self.turn = 0
+
+    def next_turn(self, time=5):
+        self.turn += 1
+        self.turns_time.append(time)
+        return self.turn
+
+    @property
+    def turn_time(self):
+        return 10 + self.turn / 10
+
+    @property
+    def average_turn_time(self):
+        if len(self.turns_time) != 0:
+            sum_time = 0
+            for turn_time in self.turns_time:
+                sum_time += turn_time
+            return round(sum_time / len(self.turns_time), 1)
+        else:
+            return -1
--- Бартенев/Практическая-3/CitiesGameTest.py
+++ Бартенев/Практическая-3/CitiesGameTest.py
@@ -1,4 +1,4 @@
-from CitiesGame import CitiesDict
+from CitiesGame import CitiesDict, Watch
 import pytest
 
 
@@ -8,6 +8,14 @@ def cities_dict():
 
     """
     return CitiesDict()
+
+
+@pytest.fixture
+def watch():
+    """
+
+    """
+    return Watch()
 
 
 def test_is_city(cities_dict):
@@ -42,3 +50,55 @@ def test_select_city(cities_dict):
     assert cities_dict.select_city("Москва") == True
     assert cities_dict.select_city("Стол") == False
     assert cities_dict.select_city("Стул") == False
+
+
+def test_get_next_char(cities_dict):
+    """
+
+    """
+    assert cities_dict.get_next_char("Архангельск") == "К"
+    assert cities_dict.get_next_char("Москва") == "А"
+    assert cities_dict.get_next_char("Чаны") == "Н"
+    assert cities_dict.get_next_char("Пермь") == "М"
+    assert cities_dict.get_next_char("Калуга") == "А"
+
+
+def test_next_turn(watch):
+    """
+
+    """
+    assert watch.next_turn() == 1
+    assert watch.next_turn() == 2
+    assert watch.next_turn() == 3
+    assert watch.next_turn() == 4
+    assert watch.next_turn() == 5
+
+
+def test_turn_time(watch):
+    """
+
+    """
+    assert watch.turn_time == 10
+    watch.next_turn()
+    assert watch.turn_time == 10.1
+    watch.next_turn()
+    assert watch.turn_time == 10.2
+    watch.next_turn()
+    assert watch.turn_time == 10.3
+    watch.next_turn()
+    assert watch.turn_time == 10.4
+
+
+def test_average_turn_time(watch):
+    """
+
+    """
+    assert watch.average_turn_time == -1
+    watch.next_turn()
+    assert watch.average_turn_time == 5
+    watch.next_turn(6)
+    assert watch.average_turn_time == 5.5
+    watch.next_turn(7)
+    assert watch.average_turn_time == 6
+    watch.next_turn(4)
+    assert watch.average_turn_time == 5.5
