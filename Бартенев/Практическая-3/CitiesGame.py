09dc106dea9150f8a006307b13d8b5f85d6ec131
Flamer322 <b.bar1enev@gmail.com>
2021-10-05 11:23:51 +0300

    Added Game, changed cities

--- Бартенев/Практическая-3/CitiesGame.py
+++ Бартенев/Практическая-3/CitiesGame.py
@@ -1,20 +1,24 @@
+import time
+
+
 class CitiesDict:
     def __init__(self):
         self.cities = {}
-        with open("cities.txt", "r", encoding='utf-8') as read_file:
+        with open('cities.txt', 'r', encoding='utf-8') as read_file:
             for city in read_file:
+                city = city.lower()
                 self.cities[city[:-1]] = True
 
     def is_city(self, city):
+        city = city.lower()
         return city in self.cities
 
     def is_available(self, city):
-        if self.is_city(city):
-            return self.cities.get(city)
-        else:
-            return False
+        city = city.lower()
+        return self.cities.get(city)
 
     def select_city(self, city):
+        city = city.lower()
         if self.is_available(city):
             self.cities[city] = False
             return True
@@ -30,14 +34,21 @@ class CitiesDict:
 
 
 class Watch:
-    def __init__(self):
-        self.turns_time = []
+    def __init__(self, players):
+        self.players = players
+        self.turn_times = []
         self.turn = 0
+        self.start_time = time.perf_counter()
 
-    def next_turn(self, time=5):
+    def next_turn(self, turn_time):
         self.turn += 1
-        self.turns_time.append(time)
+        self.turn_times.append(turn_time)
         return self.turn
+
+    def get_turn_time(self):
+        turn_time = round(time.perf_counter() - self.start_time, 1)
+        self.start_time = time.perf_counter()
+        return turn_time
 
     @property
     def turn_time(self):
@@ -45,10 +56,49 @@ class Watch:
 
     @property
     def average_turn_time(self):
-        if len(self.turns_time) != 0:
-            sum_time = 0
-            for turn_time in self.turns_time:
-                sum_time += turn_time
-            return round(sum_time / len(self.turns_time), 1)
+        sum_time = []
+        n = []
+        avg_time = []
+        for i in range(self.players):
+            sum_time.append(0)
+            n.append(0)
+        for i in range(0, len(self.turn_times)):
+            sum_time[i % self.players] += self.turn_times[i]
+            n[i % self.players] += 1
+        for i in range(0, 2):
+            if n[i] == 0:
+                avg_time.append(-1)
+            else:
+                avg_time.append(round(sum_time[i] / n[i], 1))
+        return avg_time
+
+
+class Game:
+    def __init__(self, players):
+        self.dict = CitiesDict()
+        self.watch = Watch(players)
+        self.players = players
+        self.currentPlayer = 0
+        self.lastChar = ''
+
+    def check_turn(self, city):
+        if self.dict.is_city(city):
+            if self.dict.is_available(city):
+                if self.lastChar == '' or self.lastChar == city[0].upper():
+                    self.make_turn(city)
+                    return 1
+            else:
+                return 0
         else:
             return -1
+
+    def make_turn(self, city):
+        self.dict.select_city(city)
+        self.lastChar = self.dict.get_next_char(city)
+        self.currentPlayer = (self.currentPlayer + 1) % self.players
+        turn_time = self.watch.get_turn_time()
+        self.watch.next_turn(turn_time)
+        return self.lastChar
+
+    def get_current_player(self):
+        return self.currentPlayer
--- Бартенев/Практическая-3/CitiesGameTest.py
+++ Бартенев/Практическая-3/CitiesGameTest.py
@@ -1,4 +1,6 @@
-from CitiesGame import CitiesDict, Watch
+import time
+
+from CitiesGame import CitiesDict, Watch, Game
 import pytest
 
 
@@ -15,63 +17,85 @@ def watch():
     """
 
     """
-    return Watch()
+    return Watch(2)
+
+
+@pytest.fixture
+def game():
+    """
+
+    """
+    return Game(2)
 
 
 def test_is_city(cities_dict):
     """
 
     """
-    assert cities_dict.is_city("Архангельск") == True
-    assert cities_dict.is_city("Москва") == True
-    assert cities_dict.is_city("Варшава") == True
-    assert cities_dict.is_city("Стол") == False
-    assert cities_dict.is_city("Стул") == False
+    assert cities_dict.is_city('Архангельск') == True
+    assert cities_dict.is_city('Москва') == True
+    assert cities_dict.is_city('Варшава') == True
+    assert cities_dict.is_city('Стол') == False
+    assert cities_dict.is_city('Стул') == False
 
 
 def test_is_available(cities_dict):
     """
 
     """
-    assert cities_dict.select_city("Архангельск") == True
-    cities_dict.select_city("Архангельск")
-    assert cities_dict.select_city("Архангельск") == False
-    assert cities_dict.select_city("Москва") == True
-    assert cities_dict.select_city("Стол") == False
-    assert cities_dict.select_city("Стул") == False
+    assert cities_dict.select_city('Архангельск') == True
+    cities_dict.select_city('Архангельск')
+    assert cities_dict.select_city('Архангельск') == False
+    assert cities_dict.select_city('Москва') == True
+    assert cities_dict.select_city('Стол') == False
+    assert cities_dict.select_city('Стул') == False
 
 
 def test_select_city(cities_dict):
     """
 
     """
-    assert cities_dict.select_city("Архангельск") == True
-    assert cities_dict.select_city("Архангельск") == False
-    assert cities_dict.select_city("Москва") == True
-    assert cities_dict.select_city("Стол") == False
-    assert cities_dict.select_city("Стул") == False
+    assert cities_dict.select_city('Архангельск') == True
+    assert cities_dict.select_city('Архангельск') == False
+    assert cities_dict.select_city('Москва') == True
+    assert cities_dict.select_city('Санкт-Петербург') == True
+    assert cities_dict.select_city('Москва') == False
 
 
 def test_get_next_char(cities_dict):
     """
 
     """
-    assert cities_dict.get_next_char("Архангельск") == "К"
-    assert cities_dict.get_next_char("Москва") == "А"
-    assert cities_dict.get_next_char("Чаны") == "Н"
-    assert cities_dict.get_next_char("Пермь") == "М"
-    assert cities_dict.get_next_char("Калуга") == "А"
+    assert cities_dict.get_next_char('Архангельск') == 'К'
+    assert cities_dict.get_next_char('Москва') == 'А'
+    assert cities_dict.get_next_char('Чаны') == 'Н'
+    assert cities_dict.get_next_char('Пермь') == 'М'
+    assert cities_dict.get_next_char('Калуга') == 'А'
 
 
 def test_next_turn(watch):
     """
 
     """
-    assert watch.next_turn() == 1
-    assert watch.next_turn() == 2
-    assert watch.next_turn() == 3
-    assert watch.next_turn() == 4
-    assert watch.next_turn() == 5
+    assert watch.next_turn(5) == 1
+    assert watch.next_turn(5) == 2
+    assert watch.next_turn(5) == 3
+    assert watch.next_turn(5) == 4
+    assert watch.next_turn(5) == 5
+
+
+def test_get_turn_time(watch):
+    """
+
+    """
+    # assert watch.get_turn_time() == 0
+    # time.sleep(1)
+    # assert watch.get_turn_time() == 1
+    # time.sleep(0.5)
+    # assert watch.get_turn_time() == 0.5
+    # time.sleep(1)
+    # assert watch.get_turn_time() == 1
+    # assert watch.get_turn_time() == 0
 
 
 def test_turn_time(watch):
@@ -79,13 +103,13 @@ def test_turn_time(watch):
 
     """
     assert watch.turn_time == 10
-    watch.next_turn()
+    watch.next_turn(5)
     assert watch.turn_time == 10.1
-    watch.next_turn()
+    watch.next_turn(5)
     assert watch.turn_time == 10.2
-    watch.next_turn()
+    watch.next_turn(5)
     assert watch.turn_time == 10.3
-    watch.next_turn()
+    watch.next_turn(5)
     assert watch.turn_time == 10.4
 
 
@@ -93,12 +117,52 @@ def test_average_turn_time(watch):
     """
 
     """
-    assert watch.average_turn_time == -1
-    watch.next_turn()
-    assert watch.average_turn_time == 5
+    assert watch.average_turn_time == [-1, -1]
+    watch.next_turn(5)
+    assert watch.average_turn_time == [5, -1]
     watch.next_turn(6)
-    assert watch.average_turn_time == 5.5
+    assert watch.average_turn_time == [5, 6]
     watch.next_turn(7)
-    assert watch.average_turn_time == 6
-    watch.next_turn(4)
-    assert watch.average_turn_time == 5.5
+    assert watch.average_turn_time == [6, 6]
+    watch.next_turn(8)
+    assert watch.average_turn_time == [6, 7]
+
+
+def test_make_turn(game):
+    """
+
+    """
+    assert game.make_turn('Кабардинск') == 'К'
+    assert game.make_turn('Калуга') == 'А'
+    assert game.make_turn('Анталия') == 'Я'
+    assert game.make_turn('Якутск') == 'К'
+    assert game.make_turn('Керчь') == 'Ч'
+
+
+def test_check_turn(game):
+    """
+
+    """
+    assert game.check_turn('Стул') == -1
+    assert game.check_turn('Чаны') == 1
+    assert game.check_turn('Нальчик') == 1
+    assert game.check_turn('Кукла') == -1
+    assert game.check_turn('Казань') == 1
+    assert game.check_turn('Нальчик') == 0
+
+
+def test_get_current_player(game):
+    """
+
+    """
+    assert game.get_current_player() == 0
+    game.make_turn('Кабардинск')
+    assert game.get_current_player() == 1
+    game.make_turn('Калуга')
+    assert game.get_current_player() == 0
+    game.make_turn('Анталия')
+    assert game.get_current_player() == 1
+    game.make_turn('Якутск')
+    assert game.get_current_player() == 0
+    game.make_turn('Керчь')
+    assert game.get_current_player() == 1
